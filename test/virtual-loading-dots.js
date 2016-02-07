1850a0eaf42f4084ed9f33a49e63a7602e7e27c7
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-02-07 12:25:15 -0500

    Update tests

--- test/virtual-loading-dots.js
+++ test/virtual-loading-dots.js
@@ -4,8 +4,8 @@ var h = require('virtual-dom').h
 
 // For now let's just test that it doesn't break in node/browser
 test('test running in ' + (process.browser ? 'browser' : 'node'), function (t) {
-  var loadingDots = LoadingDots('red', 18, 'white', '25%')
-  loadingDots.render(h)
+  LoadingDots.render(h)
+  LoadingDots.render(h, {color: 'red', borderRadius: 50})
   t.pass()
   t.end()
 })
