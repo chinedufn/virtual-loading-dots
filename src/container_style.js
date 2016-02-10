c0955df194ed3cee78db1280838623a29e168eee
Ben Drucker <bvdrucker@gmail.com>
2016-02-10 07:51:12 -0800

    Add vendor prefixing for flexbox styles

--- src/container_style.js
+++ src/container_style.js
@@ -1,11 +1,13 @@
+var prefix = require('preflex')
+
 // The three containers that house our dots
-var DotContainerStyle = {
+var DotContainerStyle = prefix({
   // width: This gets set during rendering
   alignItems: 'center',
   display: 'flex',
   flex: '0 0 auto',
   height: '100%',
   justifyContent: 'space-around'
-}
+})
 
 module.exports = DotContainerStyle
